838b05db39df2d7e8b845dc04c34382c1cbeed45
saju575 <sakilahmedsaju575@gmail.com>
2024-01-05 19:43:29 +0600

    Project initialize

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Render the app into the root element
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* Use the Provider to provide the Redux store to the app */}
+  </React.StrictMode>
+);
